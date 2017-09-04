757736ef016fd35302a13a6937f8bab7d7c7f332
Piotr Stefanowicz <stefan8357@gmail.com>
2017-09-04 23:08:15 +0200

    displaying 4 more tiles after clicking contact button

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-$('.slider--tile').map((index, tile) => {
-  index > 3 ? $(tile).addClass('slider--tile-hidden') : null
+let currentPage = 1;
+const tiles = $('.slider--tile')
+const amountOfTiles = tiles.length
+
+const pageDisplay = currentPage => tiles.map((index, tile) => {
+  $(tile).removeClass('slider--tile-hidden')
+  index > (currentPage * 4) - 1 ? $(tile).addClass('slider--tile-hidden') : null
+})
+
+pageDisplay(currentPage)
+
+$('.contact--button').click(function () {
+  if (currentPage < (amountOfTiles / 4)) {
+    currentPage += 1;
+    pageDisplay(currentPage)
+  }
 })
