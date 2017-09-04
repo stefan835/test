f05dc2c82c270ef9779ec1bc7904a1bbf43a96db
Piotr Stefanowicz <stefan8357@gmail.com>
2017-09-05 01:12:52 +0200

    changing pages after clicking arrows

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
+//SLIDER
 let currentPage = 0;
-const tiles = $('.slider--tile')
-const amountOfTiles = tiles.length
+const tiles = $('.slider--tile');
+const amountOfTiles = tiles.length;
 
 const pageDisplay = currentPage => tiles.map((index, tile) => {
-  $(tile).addClass('slider--tile-hidden')
+  $(tile).addClass('slider--tile-hidden');
   index > currentPage * 4 - 1 && index < currentPage * 4 + 4 ? $(tile).removeClass('slider--tile-hidden') : null
-})
+});
 
-pageDisplay(currentPage)
+pageDisplay(currentPage);
 
 const rotateSlider = delta => {
-  (currentPage < (amountOfTiles / 4) - 1) ? currentPage += delta : null
+  currentPage < amountOfTiles / 4 && (currentPage >= 0) ? currentPage += delta : null;
   pageDisplay(currentPage)
-}
+};
 
-$('.contact--button').on("click", () => {
-  rotateSlider(1)
-})
+$('.arrow--right').on("click", () => {
+  rotateSlider(1);
+  $('.arrow--left').show();
+  currentPage === (amountOfTiles / 4) - 1 ? $('.arrow--right').hide() : null
+});
+
+$('.arrow--left').on("click", () => {
+  rotateSlider(-1);
+  $('.arrow--right').show();
+  currentPage === 0 ? $('.arrow--left').hide() : null
+});
+
+//LINKS
+
+
 
 
 
