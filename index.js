a51e5d04b13c358c1e0796c950a0ed55d3f3759e
Piotr Stefanowicz <stefan8357@gmail.com>
2017-09-05 07:03:14 +0200

    adding slider buttons, marking current slide

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
-//SLIDER
 let currentPage = 0;
 const tiles = $('.slider--tile');
 const amountOfTiles = tiles.length;
+const tilesOnSlide = 4;
+const amountOfSlides = Math.ceil(amountOfTiles / tilesOnSlide);
 
 const pageDisplay = currentPage => tiles.map((index, tile) => {
   $(tile).addClass('slider--tile-hidden');
-  index > currentPage * 4 - 1 && index < currentPage * 4 + 4 ? $(tile).removeClass('slider--tile-hidden') : null
+  index > currentPage * tilesOnSlide - 1 && index < currentPage * tilesOnSlide + tilesOnSlide ? $(tile).removeClass('slider--tile-hidden') : null
 });
 
 pageDisplay(currentPage);
 
 const rotateSlider = delta => {
-  currentPage < amountOfTiles / 4 && (currentPage >= 0) ? currentPage += delta : null;
-  pageDisplay(currentPage)
+  currentPage < amountOfSlides && (currentPage >= 0) ? currentPage += delta : null;
+  pageDisplay(currentPage);
+  highlightPage()
 };
 
-$('.arrow--right').on("click", () => {
+$('.slider--arrow__right').on("click", () => {
   rotateSlider(1);
-  $('.arrow--left').show();
-  currentPage === (amountOfTiles / 4) - 1 ? $('.arrow--right').hide() : null
+  $('.slider--arrow__left').show();
+  currentPage === (amountOfTiles / 4) - 1 ? $('.slider--arrow__right').hide() : null
 });
 
-$('.arrow--left').on("click", () => {
+$('.slider--arrow__left').on("click", () => {
   rotateSlider(-1);
-  $('.arrow--right').show();
-  currentPage === 0 ? $('.arrow--left').hide() : null
+  $('.slider--arrow__right').show();
+  currentPage === 0 ? $('.slider--arrow__left').hide() : null
 });
 
-//LINKS
 
+for (let i = 0; i < amountOfSlides; i += 1) {
+  $('.slider--buttons').append($('<div>').addClass('slider--button').attr('data-index', i + 1))
+}
 
-
-
-
+const highlightPage = () => {
+  $('.slider--button').map((index, button) => {
+    console.log(button);
+    console.log($(button));
+    index === currentPage ? $(button).addClass('slider--button__active') : $(button).removeClass('slider--button__active')
+  })
+};
+highlightPage();
